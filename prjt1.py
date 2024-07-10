45b2860588307e0824f4fbaa352377c240264bd4
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-07-10 10:02:11 +0530

    new file added

--- prjt1.py
+++ prjt1.py
@@ -1,4 +1,5 @@
 bookings=[]
+
 while True:
     print("1.add a new tourist destination \n 2.book a tour \n 3.view all bookings \n 4.exit \n")
 
@@ -7,12 +8,13 @@ while True:
     if ch=='1':
         name=input("enter the name of the destination:")
         country=input("enter the country:")
-    
+        
     elif ch=='2':
         print("\nbook a tour:")
         name=input("enter your name:")
         destination=input("enter the destination you want to book:")
-        bookings.append({"name":name,"destination":destination})
+        booking_date = input("Enter the booking date (YYYY-MM-DD): ")
+        bookings.append({"name":name,"destination":destination, "booking_date": booking_date})
         print("booking successful\n")
     
     elif ch=='3':
@@ -21,24 +23,11 @@ while True:
             print("no booking yet\n")
         else:
             for booking in bookings:
-                print(f"Name: {booking['name']}, Destination: {booking['destination']}")
+                print(f"Name: {booking['name']}, Destination: {booking['destination']}, Booking Date: {booking['booking_date']}")
             print()
 
-    elif ch == '4':
-        cancel_name = input("Enter name to cancel booking: ")
-        cancel_destination = input("Enter destination to cancel booking: ")
-        for booking in bookings:
-            found=False
-            if booking['name'].lower() == cancel_name.lower() and booking['destination'].lower() == cancel_destination.lower():
-                bookings.remove(booking)
-                found=True
-                print("Booking for {cancel_name} to {cancel_destination} cancelled.\n")
-                break
-        
-        if not found:
-            print("Booking not found for {cancel_name} to {cancel_destination}\n")
     
-    elif ch == '5':
+    elif ch == '4':
         print("\nExiting the system.")
         break
     
